5cc4cc1892d29775d159d405a5694ee4aab833f4
gottumukkalakiran <gkirang4@gmail.com>
2022-11-28 11:58:16 +0000

    hamburger menu

---
+++ src/components/Header/index.js
@@ -0,0 +1,58 @@
+import Popup from 'reactjs-popup'
+import {withRouter, Link} from 'react-router-dom'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoMdClose} from 'react-icons/io'
+import {AiFillHome} from 'react-icons/ai'
+import {BsInfoCircleFill} from 'react-icons/bs'
+import './index.css'
+
+const Header = () => (
+  <div className="nav-header">
+    <div className="nav-content">
+      <Link to="/">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+          alt="website logo"
+        />
+      </Link>
+      <Popup
+        modal
+        trigger={
+          <button type="button" className="hamburger-icon-button">
+            <GiHamburgerMenu size="30" />
+          </button>
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              onClick={() => close()}
+            >
+              <IoMdClose size="30" color="#616e7c" />
+            </button>
+            <ul className="nav-link-list">
+              <li className="nav-link-item">
+                <Link className="nav-link" to="/" onClick={() => close()}>
+                  <AiFillHome size="36" />
+                  <h1 className="nav-link-content">Home</h1>
+                </Link>
+              </li>
+              <li className="nav-link-item">
+                <Link className="nav-link" to="/about" onClick={() => close()}>
+                  <BsInfoCircleFill size="32" />
+                  <h1 className="nav-link-content">About</h1>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </Popup>
+    </div>
+  </div>
+)
+
+export default withRouter(Header)
